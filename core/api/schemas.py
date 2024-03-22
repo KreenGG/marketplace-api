d38349d3fa58f606f66ccbc6b2aabdf4015f8ba7
Kirill Shugaev <kira.krut@yandex.ru>
2024-03-22 16:24:21 +0500

    added product service and schemas for api response

--- core/api/schemas.py
+++ core/api/schemas.py
@@ -1,5 +1,27 @@
-from pydantic import BaseModel
+from typing import Any, Generic, List, TypeVar
+from pydantic import Field
+
+from ninja import Schema
+
+from core.api.filters import PaginationOut
 
 
-class PingResponseSchema(BaseModel):
+TListItem = TypeVar("TListItem")
+TData = TypeVar("TData")
+
+class PingResponseSchema(Schema):
     result: bool
+    
+    
+class ListPaginatedResponse(Schema, Generic[TListItem]):
+    items: List[TListItem]
+    pagination: PaginationOut
+    
+    
+class ApiResponse(Schema, Generic[TData]):
+    data: TData | dict = Field(default_factory=dict)
+    meta: dict[str, Any] | dict = Field(default_factory=dict)
+    errors: List[Any] | dict = Field(default_factory=dict)
+
+
+
